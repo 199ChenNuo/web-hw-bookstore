1fdceb4ceea33d9393f060739481f10662f8643f
199ChenNuo <cn.222@sjtu.edu.cn>
2018-04-13 00:00:16 +0800

    menu

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,53 +1,74 @@
 import React, {Component} from 'react';
-import { Carousel } from 'antd';
-import book1 from '../images/book1.jpg'
-import timg from '../images/timg.jpg'
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
 
-function onChange(a, b, c) {
-  console.log(a, b, c);
+class Home extends React.Component {
+  state = {
+    collapsed: false,
+  };
+  onCollapse = (collapsed) => {
+    console.log(collapsed);
+    this.setState({ collapsed });
+  }
+  render() {
+    return (
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          collapsible
+          collapsed={this.state.collapsed}
+          onCollapse={this.onCollapse}
+        >
+          <div className="logo" />
+          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+            <Menu.Item key="1">
+              <Icon type="pie-chart" />
+              <span>Option 1</span>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Icon type="desktop" />
+              <span>Option 2</span>
+            </Menu.Item>
+            <SubMenu
+              key="sub1"
+              title={<span><Icon type="user" /><span>User</span></span>}
+            >
+              <Menu.Item key="3">Tom</Menu.Item>
+              <Menu.Item key="4">Bill</Menu.Item>
+              <Menu.Item key="5">Alex</Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="sub2"
+              title={<span><Icon type="team" /><span>Team</span></span>}
+            >
+              <Menu.Item key="6">Team 1</Menu.Item>
+              <Menu.Item key="8">Team 2</Menu.Item>
+            </SubMenu>
+            <Menu.Item key="9">
+              <Icon type="file" />
+              <span>File</span>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }} />
+          <Content style={{ margin: '0 16px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+              <Breadcrumb.Item>User</Breadcrumb.Item>
+              <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+              Bill is a cat.
+            </div>
+          </Content>
+          <Footer style={{ textAlign: 'center' }}>
+            Ant Design ©2016 Created by Ant UED
+          </Footer>
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
-class Home extends Component{
-    render(){
-        return (
-            <div>
-                <h1 class="home-title">推荐书目</h1>
-                <div></div>
-                <div style={{alignItems:'center'}}>
-                <Carousel afterChange={onChange}>
-                    <div>
-                        <h1>岛上书店</h1>
-                        
-                        <img src={book1}
-                        style={{margin:'0 auto'}}/>
-                        <h3>1</h3>
-                    </div>
-                    <div>
-                        <h1>岛上书店</h1>
-                        
-                        <img src={book1}
-                        style={{margin:'0 auto'}}/>
-                        <h3>2</h3>
-                    </div>
-                    <div>
-                        <h1>唐顿庄园</h1>
-                        
-                        <img src={timg}
-                        style={{margin:'0 auto'}}/>
-                        <h3>3</h3>
-                    </div>
-                    <div>
-                        <h1>唐顿庄园</h1>
-                        
-                        <img src={timg}
-                        style={{margin:'0 auto'}}/>
-                        <h3>4</h3>
-                    </div>
-                </Carousel>
-                </div>
-             </div>
-        )
-    }
-}
 
 export default Home;
---
+++ src/components/home/home2.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import { Carousel } from 'antd';
+import book1 from '../images/book1.jpg'
+import timg from '../images/timg.jpg'
+
+function onChange(a, b, c) {
+  console.log(a, b, c);
+}
+
+class Home extends Component{
+    render(){
+        return (
+            <div>
+                <h1 class="home-title">推荐书目</h1>
+                <div></div>
+                <div style={{alignItems:'center'}}>
+                <Carousel afterChange={onChange}>
+                    <div>
+                        <h1>岛上书店</h1>
+                        
+                        <img src={book1}
+                        style={{margin:'0 auto'}}/>
+                        <h3>1</h3>
+                    </div>
+                    <div>
+                        <h1>岛上书店</h1>
+                        
+                        <img src={book1}
+                        style={{margin:'0 auto'}}/>
+                        <h3>2</h3>
+                    </div>
+                    <div>
+                        <h1>唐顿庄园</h1>
+                        
+                        <img src={timg}
+                        style={{margin:'0 auto'}}/>
+                        <h3>3</h3>
+                    </div>
+                    <div>
+                        <h1>唐顿庄园</h1>
+                        
+                        <img src={timg}
+                        style={{margin:'0 auto'}}/>
+                        <h3>4</h3>
+                    </div>
+                </Carousel>
+                </div>
+             </div>
+        )
+    }
+}
+
+export default Home;
